b8f81965f3d47b11daba077979cbbd518c42990a
Lex Nousagi <alexmiyin@gmail.com>
2022-12-01 13:56:14 +0200

    feat(parchment): add component

---
+++ components/parchment.tsx
@@ -0,0 +1,35 @@
+import Image from 'next/image'
+import React from 'react'
+
+interface Props {
+  children: React.ReactNode
+}
+
+export default function Parchment({ children }: Props) {
+  return (
+    <div className="min-h-screen relative">
+      <Image
+        src="/parchment.png"
+        alt="Parchment Background"
+        width={2200}
+        height={1900}
+        className="absolute inset-0 w-full h-full -z-10"
+      />
+      <Image
+        src="/steam-punk-icon-1.png"
+        alt="Steam Punk Mechanism"
+        width={116}
+        height={116}
+        className="absolute bottom-4 right-4"
+      />
+      <Image
+        src="/steam-punk-icon-2.png"
+        alt="Steam Punk Unknown Item"
+        width={116}
+        height={116}
+        className="absolute bottom-4 left-4"
+      />
+      <div className="mx-4 md:mx-24">{children}</div>
+    </div>
+  )
+}
